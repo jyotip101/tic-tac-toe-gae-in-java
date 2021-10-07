9d67ab1270d89c298db6846d4aa9b7a4ca661188
jyotip101 <jyotipatelfyit77100@gmail.com>
2021-10-07 22:22:51 +0530

    commit

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -7,67 +7,110 @@ public class TicTacToe implements ActionListener{
 
 	Random random = new Random();
 	JFrame frame = new JFrame("Tic-Tac-Toe");
-	JPanel titlePanel = new JPanel();
-	JPanel buttonPanel = new JPanel();
-	JLabel textLabel = new JLabel();
-	JLabel playerTurnLabel = new JLabel();
+	
+	JPanel titlePanel1 = new JPanel();
+	JPanel titlePanel2 = new JPanel();
+	
+	JPanel buttonPanel = new JPanel(); 
+	
+	JLabel textLabel1 = new JLabel();
+	JLabel textLabel2 = new JLabel();
+	
 	JButton[] buttons = new JButton[9];
-	boolean playerTurn;
+	boolean player;
 	
 	TicTacToe() { 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500, 600);
-//		frame.getContentPane().setBackground(new Color(50, 50, 50));
+		frame.setSize(400, 500);
+		frame.getContentPane().setBackground(new Color(50, 50, 50));
 		frame.setLayout(new BorderLayout());
 		frame.setVisible(true);
 		
-		textLabel.setBackground(new Color(25, 25, 25));
-//		textLabel.setForeground(new Color(25, 255, 0));
-		textLabel.setForeground(new Color(251, 108, 109));
-		textLabel.setFont(new Font("Ink Free", Font.BOLD, 75));
-//		textLabel.setHorizontalAlignment(JLabel.CENTER);
-		textLabel.setText("Tic-Tac-Toe");
-		textLabel.setOpaque(true);
+		textLabel1.setBackground(new Color(25, 25, 25)); 
+		textLabel1.setForeground(new Color(251, 108, 109));
+		textLabel1.setFont(new Font("Ink Free", Font.BOLD, 45));
+		textLabel1.setHorizontalAlignment(JLabel.CENTER);
+		textLabel1.setText("Tic-Tac-Toe");
+		textLabel1.setOpaque(true);
 		
-		titlePanel.setLayout(new BorderLayout());
-		titlePanel.setBounds(10, 0, 500, 200);
+		textLabel2.setBackground(new Color(25, 25, 25)); 
+		textLabel2.setForeground(new Color(251, 108, 109));
+		textLabel2.setFont(new Font("Ink Free", Font.BOLD, 20));
+		textLabel2.setHorizontalAlignment(JLabel.CENTER);
+		textLabel2.setText("Starting...");
+		textLabel2.setOpaque(true);
 		
-//		playerTurnLabel.setLayout(new BorderLayout());
-//		playerTurnLabel.setBounds(0, 100, 800, 100);
-		playerTurnLabel.setText("Tic-Tac-Toe2");
+		titlePanel1.setLayout(new BorderLayout());
+		titlePanel1.setBounds(0, 0, 400, 50); 
+		
+		titlePanel2.setLayout(new BorderLayout());
+		titlePanel2.setBounds(0, 50, 400, 40); 
 		
 		buttonPanel.setLayout(new GridLayout(3, 3));
 		buttonPanel.setBackground(new Color(22, 24, 30));
-//		
-//		for(int i=0; i<9; i++) {
-//			buttons[i] = new JButton();
-//			buttonPanel.add(buttons[i]);
-//			buttons[i].setFont(new Font("MV Boil", Font.BOLD, 120));
-////			buttons[i].setText("" + (i + 1)); 
-////			font color buttons[i].setBackground(new Color(61, 64, 91));
-////		background	buttons[i].setBackground(new Color(244, 241, 222));
-////			buttons[i].setBackground(new Color(221, 147, 86));
-//
-//			buttons[i].setBackground(new Color(157, 166, 182));
-//			buttons[i].setFocusable(false);
-//			buttons[i].addActionListener(this);
-//		}
 		
-		titlePanel.add(textLabel);
-		titlePanel.add(playerTurnLabel);
-		frame.add(titlePanel, BorderLayout.NORTH);
-		frame.add(buttonPanel);
+		for(int i=0; i<9; i++) {
+			buttons[i] = new JButton();
+			buttonPanel.add(buttons[i]);
+			buttons[i].setFont(new Font("MV Boil", Font.BOLD, 80));
+//			buttons[i].setText("" + (i + 1));  
+//			buttons[i].setForeground(new Color(25, 25, 25)); 
+
+//			buttons[i].setBackground(new Color(157, 166, 182));
+			buttons[i].setFocusable(false);
+			buttons[i].addActionListener(this);
+		}
+		
+		titlePanel1.add(textLabel1);
+		titlePanel2.add(textLabel2);
+		frame.add(titlePanel1, BorderLayout.NORTH);
+		frame.add(titlePanel2, BorderLayout.CENTER) ;
+		frame.add(buttonPanel );
+		oxTurn();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
-		
+//		for(int i=0; i<9; i++) {
+//			if(e.getSource() == buttons[i]) {
+//				if(player) {
+//					if(buttons[i].getText() == "") {
+//						buttons[i].setBackground(new Color(157, 166, 182));
+//						buttons[i].setText("X");
+//						player = false;
+//						textLabel2.setText("Player O");
+//						check();
+//					}
+//				}else {
+//					if(buttons[i].getText() == "") {
+//						buttons[i].setBackground(new Color(157, 166, 182));
+//						buttons[i].setText("O");
+//						player = true;
+//						textLabel2.setText("Player X");
+//						check();
+//					}
+//				}
+//			}
+//		}
 		
 	}
 	
-	public void fistTurn() {
-		
+	public void oxTurn() {
+//		
+//		try {
+//			Thread.sleep(2000);
+//		} catch (InterruptedException e) { 
+//			e.printStackTrace();
+////		}
+//		
+//		if(random.nextInt(2) == 0) { 
+//			player = true;
+//			textLabel2.setText("Player X"); 
+//		}else {
+//			player = false;
+//			textLabel2.setText("Player O");
+//		}
 	}
 	
 	public void check() {
